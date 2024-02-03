bd0ec3c2b49f46e46cab19ecb090f25069be8d6e
Sathvik C <sathvikchekkali123@gmail.com>
2024-02-03 12:08:15 +0530

    maven java

---
+++ src/main/java/se_lab/ex_java/App.java
@@ -0,0 +1,13 @@
+package se_lab.ex_java;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
